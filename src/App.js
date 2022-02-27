2895b63c2e06124b6909f633fa0742ec7ba82b5d
Sujan Mongar <99518676+Sujan-Ale@users.noreply.github.com>
2022-02-27 16:51:08 +0600

    Change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div class="back"></div>
-        <div class="heart"></div>
-      </header>
+    <div>
+      <div class="back"></div>
+      <div class="heart"></div>
     </div>
   );
 }
